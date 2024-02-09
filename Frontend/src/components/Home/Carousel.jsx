3a5b470171faa1d98bd333a273a0f4d724d52215
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-02-10 01:46:46 +0200

    added text animation to carousel

--- Frontend/src/components/Home/Carousel.jsx
+++ Frontend/src/components/Home/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-fade";
@@ -7,6 +7,7 @@ import "swiper/css/pagination";
 import { EffectFade, Navigation, Pagination } from "swiper/modules";
 import "./carousel.css";
 function Carousel() {
+  // to handle prev and next buttons
   const [swiperRef, setSwiperRef] = useState(null);
 
   const prevHandler = () => {
@@ -16,31 +17,53 @@ function Carousel() {
   const nextHandler = () => {
     swiperRef.slideNext();
   };
+  ///////////////////////////
+  // for doing the text animation
+  useEffect(() => {
+    if (swiperRef) {
+      //checking if the slide is active because if the state is true so slide is active
+      swiperRef.on("slideChange", () => {
+        const animatedElements = document.querySelectorAll(".animated"); //getting tags with class animated (the number of created elements in the loop based on the data)
+        animatedElements.forEach((element) => {
+          element.style.animation = "none"; // without it the animation will start from the center so there is no animation in the other slides only in first so i force the browser to recalculate the animation
+          requestAnimationFrame(() => {
+            element.style.animation = "fadeInUp 0.7s ease-in-out forwards";
+          });
+        });
+      });
+    }
+    return () => {
+      if (swiperRef) {
+        swiperRef.off("slideChange");
+      }
+    };
+  }, [swiperRef]);
+  ////////////////////////////////////////////
   // by seif
   const data = [
     {
       id: 1,
       img: "https://images.adsttc.com/media/images/64a2/cdae/cb9c/464f/a63a/9764/large_jpg/cairo-architecture-city-guide-exploring-the-unique-architectural-blend-of-historical-and-contemporary-in-egypts-bustling-capital_23.jpg?1688391095",
       country: "cairo",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, repellat impedit? Labore consectetur asperiores ea odio quo aperiam porro quae? Aliquid impedit velit obcaecati, ullam est perferendis rem corrupti eius!",
+      desc: "cairo is very beautiful country and i love it very much",
     },
     {
       id: 2,
       img: "https://www.osiristours.com/wp-content/uploads/2016/06/Alexandria-9-e1488051126787.jpg",
       country: "Alex",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, repellat impedit? Labore consectetur asperiores ea odio quo aperiam porro quae? Aliquid impedit velit obcaecati, ullam est perferendis rem corrupti eius!",
+      desc: "alex is beautiful and have a beautiful sea",
     },
     {
       id: 3,
       img: "https://swiperjs.com/demos/images/nature-3.jpg",
       country: "Dahab",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, repellat impedit? Labore consectetur asperiores ea odio quo aperiam porro quae? Aliquid impedit velit obcaecati, ullam est perferendis rem corrupti eius!",
+      desc: "beautiful and have mountains and sea and fish",
     },
     {
       id: 4,
       img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/10/48/07/d5.jpg",
       country: "hurghada",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, repellat impedit? Labore consectetur asperiores ea odio quo aperiam porro quae? Aliquid impedit velit obcaecati, ullam est perferendis rem corrupti eius!",
+      desc: "have a wonderfull sea and many tourists vist it every year",
     },
   ];
   return (
@@ -69,16 +92,20 @@ function Carousel() {
                     {/* //////////////// */}
                     {/* the container of the buttons and description */}
                     <div className="d-flex justify-content-between mt-5">
+                      {/* description  */}
                       <div className="carousel-texts">
-                        <h3>{el.country}</h3>
-                        <p>{el.desc}</p>
-                        <button className="explore">
-                          Explore
-                          <div className="arrow-wrapper">
-                            <div className="arrow"></div>
-                          </div>
-                        </button>
+                        <div className="animated">
+                          <h3>{el.country}</h3>
+                          <p>{el.desc}</p>
+                          <button className="explore">
+                            Explore
+                            <div className="arrow-wrapper">
+                              <div className="arrow"></div>
+                            </div>
+                          </button>
+                        </div>
                       </div>
+                      {/* description end */}
                       <div className="carousel-buttons align-self-center">
                         <button onClick={prevHandler} className="d-block">
                           {"<"}
