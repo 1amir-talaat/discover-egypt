7639b2a1a7ee13d75051293f96a9a58109e8e16a
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-02-13 18:39:18 +0200

    rendering components in home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import Carousel from "../components/Home/Carousel";
+import Hero from "../components/Home/Hero";
+import Categories from "../components/Home/Categories";
+import Experience from "../components/Home/Experience";
+import Events from "../components/Home/Events";
+import Places from "../components/Home/Places";
 
 function Home() {
-  return <div><Carousel/></div>;
+  return (
+    <>
+      <Hero />
+      <Categories />
+      <Experience />
+      <Carousel />
+      <Events />
+      <Places />
+    </>
+  );
 }
 
 export default Home;
