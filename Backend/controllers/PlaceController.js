b4d8e782088b31e22648716beb9c13e3c6383ad4
amir talaat <hsiwh2be872b@gmail.com>
2024-02-17 22:17:56 +0200

    feat(PlaceController.js): add searchPlaces function to search places by keyword  feat(routes.js): add route to search places by keyword

--- Backend/controllers/PlaceController.js
+++ Backend/controllers/PlaceController.js
@@ -33,15 +33,57 @@ const PlaceController = {
       res.status(400).json({ error: error.message });
     }
   },
+  
   getPlacesByCategory: async (req, res) => {
-    const {category}=req.params
+    const { category } = req.params;
     try {
-      const places =await Place.findAll({
-        where:{
-          category: category
+      const places = await Place.findAll({
+        where: {
+          category: category,
         },
         include: [{ model: Review }, { model: PlacesImg }],
-      })
+      });
+      const data = transformPlacesData(places);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+  searchPlaces: async (req, res) => {
+    const { keyword } = req.params;
+
+    if (!keyword.trim()) {
+      return res.status(400).json({ message: "No keyword provided for search" });
+    }
+
+    const lowerKeyword = keyword.toLowerCase();
+
+    try {
+      const places = await Place.findAll({
+        where: {
+          [Op.or]: [
+            { city_ar: { [Op.like]: `%${lowerKeyword}%` } },
+            { city_en: { [Op.like]: `%${lowerKeyword}%` } },
+            { title_ar: { [Op.like]: `%${lowerKeyword}%` } },
+            { title_en: { [Op.like]: `%${lowerKeyword}%` } },
+            { desc_ar: { [Op.like]: `%${lowerKeyword}%` } },
+            { desc_en: { [Op.like]: `%${lowerKeyword}%` } },
+            { min_price: { [Op.like]: `%${lowerKeyword}%` } },
+            { max_price: { [Op.like]: `%${lowerKeyword}%` } },
+            { place_name: { [Op.like]: `%${lowerKeyword}%` } },
+            { category: { [Op.like]: `%${lowerKeyword}%` } },
+            { sub_category: { [Op.like]: `%${lowerKeyword}%` } },
+            { location_url: { [Op.like]: `%${lowerKeyword}%` } },
+          ],
+        },
+        include: [{ model: Review }, { model: PlacesImg }],
+      });
+
+      if (places.length === 0) {
+        return res.status(404).json({ message: "No places found for the specified keyword" });
+      }
+
       const data = transformPlacesData(places);
       res.status(200).json(data);
     } catch (error) {
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/places", PlaceController.getAllPlaces);
 router.get("/city/:city", PlaceController.getPlacesByCity);
 router.get("/category/:category", PlaceController.getPlacesByCategory);
+router.get("/search/:keyword", PlaceController.searchPlaces);
 
 export default router;
