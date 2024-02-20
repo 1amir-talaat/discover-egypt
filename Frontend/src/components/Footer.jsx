7e42ba88113f50cdeae7f1dd6e58d240814b98e6
amir talaat <hsiwh2be872b@gmail.com>
2024-02-20 22:38:38 +0200

    * style(Footer.css): change color of li a to white and add !important to override other styles * feat(Footer.jsx): add support for multiple languages in footer links and update layout

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -16,118 +16,75 @@ function Footer() {
   return (
     <>
       <footer>
-        <img src={img} style={{ width: "100%"}} alt="" />
+        <img src={img} style={{ width: "100%" }} alt="" />
         <div className="container">
-          <div className="row">
-            <div className="col-3 logo-col">
-              <img src={logo} className="logo" />
-            </div>
-            <div className="col-3">
-              <hr className="footer-sub-line" />
-              <h2>{language == "ar" ? "الموسم والأحداث" : "season & Events"}</h2>
-              <ul>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "Pyramids Festival of Light" : "عرض الاهرامات للضوء"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "Abu Simbel Sun Festival" : "شمس أبو سمبل"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "Alexandria International Film Festival" : "مهرجان الإسكندرية السينمائي الدولي"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "El Gouna Film Festival" : "مهرجان الجونه للافلام"}
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-3">
-              <hr className="footer-sub-line" />
-              <h2>{language == "en" ? "Discover Egypt" : "اكتشف مصر"}</h2>
-              <ul>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "nature" : "الطبيعه"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "lakes" : "البحيرات"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "The nile" : "نهر النيل"}
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-3">
-              <hr className="footer-sub-line" />
-              <h2>{language == "en" ? "Useful informations" : "معلومات مهمه"}</h2>
-              <ul>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "About Rgypt" : "مصر"}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
-                    {language == "en" ? "important numbers" : "ارقام مهمه"}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
         </div>
-        <div className="container-fuid second-foot">
+        <div className="container-fuid second-foot text-white">
           <div className="container">
             <div className="row row-con">
-              <div className="col">
-                <h2> {language == "en" ? "powerd by" : "انشيء بواسطه"} </h2>
-                <p className="d-inline team">{language == "en" ? "Elrahala team" : "الرحاله"}</p>
-              </div>
-              <div className="col">
-                <h2>{language == "en" ? "Call Center" : "مركز المكالمات"}</h2>
-                <div className="numbers">
-                  <a style={{ fontSize: 20 }} href="tel:+01066272505">
-                    {language == "en" ? "sameh muhammad" : "سامح محمد"}
-                  </a>
-                  <a style={{ fontSize: 20 }} href="tel:+01141606848">
-                    {language == "en" ? "amir talaat" : "امير طلعت"}
-                  </a>
-                  <a style={{ fontSize: 20 }} href="tel:+01021105085">
-                    {" "}
-                    {language == "en" ? "saif ahmed" : "سيف احمد"}{" "}
-                  </a>
-                  <a style={{ fontSize: 20 }} href="tel:+01121859584">
-                    {language == "en" ? "zeyad hymen" : "زياد هيمن"}
-                  </a>
-                  <a style={{ fontSize: 20 }} href="tel:+">
-                    {language == "en" ? "basmala magdy" : "بسملة مجدي"}
-                  </a>
+              <div className="row">
+                <div className="col-3 logo-col">
+                  <img src={logo} className="logo" />
                 </div>
-              </div>
-              <div className="col">
-                <h2 style={{ fontSize: 25 }}>{language == "en" ? "Subsecribe for notifications" : "اشترك لوصول الاشعارات"}</h2>
-                <form className="form e-form" method="post">
-                  <input type="text" className="e-input" name="Email" />
-                  <input className="e-submit btn" type="submit" value={language == "en" ? "join" : "تاكيد"} />
-                </form>
-              </div>
-              <div className="col">
-                <h2>{language == "en" ? "Feedback" : "الانطباع"}</h2>
-                <form className="form e-form" method="post">
-                  <input type="email" name="Feedback" className="e-input" />
-                  <input className="e-submit btn" type="submit" value={language == "en" ? "Add Feedback" : "نشر التعليق"} />
-                </form>
+                <div className="col-3">
+                  <hr className="footer-sub-line" />
+                  <h2>{language == "ar" ? "الموسم والأحداث" : "season & Events"}</h2>
+                  <ul>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "Pyramids Festival of Light" : "عرض الاهرامات للضوء"}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "Abu Simbel Sun Festival" : "شمس أبو سمبل"}
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "El Gouna Film Festival" : "مهرجان الجونه للافلام"}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-3">
+                  <hr className="footer-sub-line" />
+                  <h2>{language == "en" ? "Discover Egypt" : "اكتشف مصر"}</h2>
+                  <ul>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "nature" : "الطبيعه"}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "lakes" : "البحيرات"}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "The nile" : "نهر النيل"}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-3">
+                  <hr className="footer-sub-line" />
+                  <h2>{language == "en" ? "Useful informations" : "معلومات مهمه"}</h2>
+                  <ul>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "About Rgypt" : "مصر"}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className={language == "en" ? "a-en" : "a-ar"}>
+                        {language == "en" ? "important numbers" : "ارقام مهمه"}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
             <div className="row">
