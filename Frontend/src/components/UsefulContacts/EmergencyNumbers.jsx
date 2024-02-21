844895b757c15370d6b1aba0d7396cc94e646d27
basmala <basmalam169@gmail.com>
2024-02-21 06:26:56 +0200

    Changing colors and adding swiper in useful contacts page

--- Frontend/src/components/UsefulContacts/EmergencyNumbers.jsx
+++ Frontend/src/components/UsefulContacts/EmergencyNumbers.jsx
@@ -11,10 +11,10 @@ function EmergencyNumbers(){
                     <div className={language=="en"?"text-section-en":"text-section-ar"}>
                         <h2>{language=="en"?"Emergency numbers ":"أرقام الطوارئ"}</h2>
                         <p><b>{language=="en"?"Police: ":"الشرطة: "}</b>122</p>
-                        <p><b>{language=="en"?"Ambulance:":" الإسعاف: "}</b>123</p>
+                        <p><b>{language=="en"?"Ambulance: ":" الإسعاف: "}</b>123</p>
                         <p><b>{language=="en"?" Fire: ":"المطافئ: "}</b>180</p>
-                        <p><b>{language=="en"?"Civil Defense:":" الدفاع المدني: "}</b>240140982401409724014099</p>
-                        <p><b>{language=="en"?"For traffic accidents:":" للحوادث المرورية: "}</b>0224021233</p>
+                        <p><b>{language=="en"?"Civil Defense: ":" الدفاع المدني: "}</b>240140982401409724014099</p>
+                        <p><b>{language=="en"?"For traffic accidents: ":" للحوادث المرورية: "}</b>0224021233</p>
                         <p><b>{language=="en"?"Tourist Police: ":" شرطة السياحة: "}</b>126</p>
                         <p><b>{language=="en"?"Traffic Police: ":" شرطة المرور: "}</b>128</p>
                         <p><b>{language=="en"?"Railway Police: ":" شرطة السكة الحديد: "}</b>145</p>
--- Frontend/src/components/UsefulContacts/UsefulNumbers.jsx
+++ Frontend/src/components/UsefulContacts/UsefulNumbers.jsx
@@ -15,26 +15,26 @@ function UsefulNumbers(){
                     <div className="card ">
                         <div className="card-title">{language=="en"?"Tourism":" الخدمات السياحية"}</div>
                         <div className="card-body">
-                            <p><b>{language=="en"?"Tourism Call Center:":"مركز الاتصال السياحي: "}</b>19654</p>
-                            <p><b>{language=="en"?"General Directorate of Passports:":" المديرية العامة للجوازات: "}</b>0227956301</p>
+                            <p><b>{language=="en"?"Tourism Call Center: ":"مركز الاتصال السياحي: "}</b>19654</p>
+                            <p><b>{language=="en"?"General Directorate of Passports: ":" المديرية العامة للجوازات: "}</b>0227956301</p>
 
                         </div>
                     </div>
                     <div className="card ">
                         <div className="card-title">{language=="en"?"Consumer services":"خدمات المستهلك"}</div>
                         <div className="card-body">
-                            <p><b>{language=="en"?"Municipal services:":"البلديات: "}</b>139</p>
-                            <p><b>{language=="en"?"Electricity company emergency:":"طوارئ الكهرباء: "}</b>121</p>
-                            <p><b>{language=="en"?"Consumer protection:":"حماية المستهلك: "}</b>19588</p>
+                            <p><b>{language=="en"?"Municipal services: ":"البلديات: "}</b>139</p>
+                            <p><b>{language=="en"?"Electricity company emergency: ":"طوارئ الكهرباء: "}</b>121</p>
+                            <p><b>{language=="en"?"Consumer protection: ":"حماية المستهلك: "}</b>19588</p>
                         </div>
                     </div>
                     <div className="card ">
                         <div className="card-title">{language=="en"?"Public security":"الأمن العام"}</div>
                         <div className="card-body">
-                            <p><b>{language=="en"?"ُEgypt Public Security:":"مديرية الأمن العام:"}</b>115</p>
-                            <p><b>{language=="en"?"Complaints and inquiries:":"الشكاوي والاستعلامات: "}</b>16528</p>
-                            <p><b>{language=="en"?"Emergency medical consultation:":"الاستشارات الطبية: "}</b>111</p>
-                            <p><b>{language=="en"?"General Directorate of Narcotics Control:":"الإدارة العامة لمكافة المخدرات: "}</b>16023</p>
+                            <p><b>{language=="en"?"ُEgypt Public Security: ":"مديرية الأمن العام:"}</b>115</p>
+                            <p><b>{language=="en"?"Complaints and inquiries: ":"الشكاوي والاستعلامات: "}</b>16528</p>
+                            <p><b>{language=="en"?"Emergency medical consultation: ":"الاستشارات الطبية: "}</b>111</p>
+                            <p><b>{language=="en"?"General Directorate of Narcotics Control: ":"إدارة مكافة المخدرات: "}</b>16023</p>
                         </div>
                     </div>
                 </div>
