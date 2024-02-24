1df155bc80c41548d119548073d0b2aeb487b806
amir talaat <hsiwh2be872b@gmail.com>
2024-02-25 01:33:18 +0200

    - chore(HelpSupport.css): reformat CSS code for better readability and consistency - feat(HelpSupport.jsx): import React and add image and title to the hero section - style(HelpSupport.jsx): update CSS for the hero section to position the title and add styling - style(HelpSupport.jsx): update CSS for the form input fields - style(HelpSupport.jsx): update CSS for the phone icon button

--- Frontend/src/components/HelpSupport/HelpSupport.jsx
+++ Frontend/src/components/HelpSupport/HelpSupport.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useLanguage } from "../../context/LanguageContext";
 import "./HelpSupport.css";
 function HelpSupport() {
@@ -5,13 +6,15 @@ function HelpSupport() {
   return (
     <>
       {/* Hero's image and title  */}
-      <div className="image-container">
-        <img
-          src="https://img.freepik.com/free-photo/confident-call-center-operator-talking-with-client_74855-4059.jpg?size=626&ext=jpg&ga=GA1.1.921307657.1671265933&semt=sph"
-          style={{ objectFit: "cover" }}
-          alt=""
-        />
-        <h1 className={language == "en" ? "image-text-en" : "image-text-ar"}>{language == "en" ? "Help & Support" : "المساعدة والدعم"}</h1>
+      <div className="image-container ">
+        <div className="img-dev position-relative">
+          <img
+            src="https://img.freepik.com/free-photo/confident-call-center-operator-talking-with-client_74855-4059.jpg?size=626&ext=jpg&ga=GA1.1.921307657.1671265933&semt=sph"
+            style={{ objectFit: "cover", width: "100%", height: "600px", objectPosition: "top" }}
+            alt=""
+          />
+          <h1 className="position-absolute display-3 text-white container">{language == "en" ? "Help & Support" : "المساعدة والدعم"}</h1>
+        </div>
       </div>
       {/* the container of the page */}
       <div className="container mt-5">
@@ -29,12 +32,7 @@ function HelpSupport() {
                 <label className="mt-4 mb-2" htmlFor="inputEmail4">
                   {language == "en" ? "First Name" : "الاسم الأول"}
                 </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="inputEmail4"
-                  placeholder={language == "en" ? "Enter First Name" : "أدخل الاسم الأول"}
-                />
+                <input type="email" className="form-control" id="inputEmail4" placeholder={language == "en" ? "Enter First Name" : "أدخل الاسم الأول"} />
               </div>
               <div className="form-group col-sm-6 col-md-6 col-lg-6">
                 <label className="mt-4 mb-2" htmlFor="inputPassword4">
@@ -126,14 +124,7 @@ function HelpSupport() {
             <div className="col-5 col-sm-4 col-md-3 mt-5 text-align-center">
               <a href="#">
                 <div style={{ backgroundColor: "#00aeff", color: "white" }}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="40"
-                    height="40"
-                    fill="currentColor"
-                    className="bi bi-telephone-fill"
-                    viewBox="0 0 16 16"
-                  >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-telephone-fill" viewBox="0 0 16 16">
                     <path
                       fillRule="evenodd"
                       d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
