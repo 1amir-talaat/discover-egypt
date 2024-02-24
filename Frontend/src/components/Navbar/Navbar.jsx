f70b550a3f121844d2d651688e62d12fc72f6439
basmala <basmalam169@gmail.com>
2024-02-25 00:41:59 +0200

    Adding help-support path in validPaths variable in navbar

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -146,6 +146,7 @@ function Navbar() {
     "/travel-tips",
     "/plan-your-trip",
     "/help-support",
+    "/important-numbers"
   ];
   var navbarClass = validPaths.includes(location.pathname) ? (scrolling ? "navbar-home" : "") : "navbar-home";
 
