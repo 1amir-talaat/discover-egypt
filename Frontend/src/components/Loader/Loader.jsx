b73a71a8716ea9d0d1d372794dbc2435c74732ca
amir talaat <hsiwh2be872b@gmail.com>
2024-02-26 23:11:57 +0200

    - feat(Loader.jsx): Update Loader component to use pyramid-loader instead of newtons-cradle - style(Loader.css): Update CSS styles for pyramid-loader component

--- Frontend/src/components/Loader/Loader.jsx
+++ Frontend/src/components/Loader/Loader.jsx
@@ -3,11 +3,14 @@ import "./loader.css";
 function Loader() {
   return (
     <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
-      <div className="newtons-cradle">
-        <div className="newtons-cradle__dot"></div>
-        <div className="newtons-cradle__dot"></div>
-        <div className="newtons-cradle__dot"></div>
-        <div className="newtons-cradle__dot"></div>
+      <div className="pyramid-loader">
+        <div className="wrapper">
+          <span className="side side1"></span>
+          <span className="side side2"></span>
+          <span className="side side3"></span>
+          <span className="side side4"></span>
+          <span className="shadow"></span>
+        </div>
       </div>
     </div>
   );
