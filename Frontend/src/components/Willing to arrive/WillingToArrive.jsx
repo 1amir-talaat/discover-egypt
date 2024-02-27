c2ae5c2f07170b65a30ca137c73b704b3eb03e64
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-02-28 00:22:33 +0200

    willing to arrive et3mlt ya sy333333

--- Frontend/src/components/Willing to arrive/WillingToArrive.jsx
+++ Frontend/src/components/Willing to arrive/WillingToArrive.jsx
@@ -8,26 +8,21 @@ function WillingToArrive() {
   return (
     <>
       <div className="d-flex align-items-center gap-3 container">
-        {willingToArrive[language].map((item) => {
+        {willingToArrive.map((item) => {
           return (
             <>
               <div className="col-3" style={{ width: 300 + "px" }}>
                 <div className="card experience-card border-0 shadow w-100 position-relative mb-5">
                   <img
                     className="card-img-top experience-card-image"
-                    src={item.Place.image}
-                    alt={item.description}
+                    src={item.place.image}
+                    alt={item.desc}
                   />
                   <div className="card-body">
                     <div className="experience-card-badge">
-                      <h5>{item.country}</h5>
+                      <h5>{item.place.city}</h5>
                     </div>
-                    <h4>{item.description}</h4>
-                    <p style={{ color: "gray", fontSize: 20 }}>
-                      {language === "en"
-                        ? `Price Start From ${item.price} $`
-                        : `السعر يبدأ من ${item.price * 4} جنيه`}
-                    </p>
+                    <p>{item.place.desc}</p>
                   </div>
                   <hr className="experience-card-hr mt-4 ms-3" />
                   <button
