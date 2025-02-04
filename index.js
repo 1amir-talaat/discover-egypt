e32240fd5f1c7eea3385dea4dd3ebbc8dcaeb1eb
1amir-talaat <hsiwh2be872b@gmail.com>
2025-02-04 21:47:20 +0200

    test

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import router from "./routes/routes.js";
 
-dotenv.config();
+try {
+  dotenv.config();
 
-const app = express();
+  const app = express();
 
-app.use(express.json({ limit: "16mb" }));
+  app.use(express.json({ limit: "16mb" }));
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-app.use(router);
+  app.use(router);
 
-app.listen(5000, () => console.log("Server running at port 5000"));
+  app.listen(5000, () => console.log("Server running at port 5000"));
+} catch (error) {
+  console.log(error);
+}
